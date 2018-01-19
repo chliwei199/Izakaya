ebb66dae0a4d9df5f0586ed48caed9bbe025ded6
DarrenPeng <Darren.Peng@acer.com>
2018-01-19 16:35:15 +0800

    #php_code

--- web/event/message_event/no_event.php
+++ web/event/message_event/no_event.php
@@ -1,18 +1,10 @@
 ﻿ 
 <?php
-$redis->updateUserStatus($user_id,'');
-			$textMessage = emoji(100086).'抱歉，我不清楚您說什麼，沒辦法回答您可利用下方的圖示點選需要的服務。'.emoji(100094);
+
+			$textMessage = emoji(100010).' 抱歉，小BLAH不了解您說什麼，沒辦法回答您。';
  			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMessage);  
-	
-			$imagemap= new App\event\message_event\ImagemapHandler;				//create imagemap object
-			$ImageMessageBuilder=$imagemap->createImagemap();
  
 
-			$MultiMessageBuilder = new LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
-	
-			$MultiMessageBuilder->add($textMessageBuilder);
-			$MultiMessageBuilder->add($ImageMessageBuilder);
-
-			$response =  $bot->replyMessage($reply_token, $MultiMessageBuilder);
+			$response =  $bot->replyMessage($reply_token, $textMessageBuilder);
 ?>
  
