679fcbd8095ea6b1ead0396e358124ff31343f59
DarrenPeng <Darren.Peng@acer.com>
2018-01-19 17:39:32 +0800

    #php_code

--- web/event/message_event/no_event.php
+++ web/event/message_event/no_event.php
@@ -3,7 +3,7 @@
 
 			$textMessage = emoji(100010).' 抱歉，小BLAH不了解您說什麼，沒辦法回答您。';
  			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMessage);  
- 
+  
 
 			$response =  $bot->replyMessage($reply_token, $textMessageBuilder);
 ?>
